1dd475cd74a6b330627ebafbce42588b511326a1
Howon Lee <hlee.howon@gmail.com>
2013-06-15 19:47:54 -0700

    nit fix

--- server/publish.js
+++ server/publish.js
@@ -12,7 +12,7 @@ Meteor.publish("edges", function(){
 Meteor.methods({
 	vote: function(num, edge){
 			  console.log("VOTE COUNTED: " + num + " weight, to edge " + JSON.stringify(edge._id));
-			  Edges.update(edge._id, {$inc: {score: num, voters: 1}});
+			  Edges.update(edge._id, {$inc: {score: num, votes: 1}});
 			  console.log(JSON.stringify(Edges.find(edge._id).fetch()));
 			  return 1;
 		  },
