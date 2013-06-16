45be2b252a190a02b28151560dc0a6d086662400
Howon Lee <hlee.howon@gmail.com>
2013-06-16 00:22:25 -0700

    yeah I put binaries in git repos what are you gonna do about it

--- server/publish.js
+++ server/publish.js
@@ -11,58 +11,58 @@ Meteor.publish("edges", function(){
 
 var nodeStateInfo = [
 			[
-				{name: "Breathing", obj: "A breath of air when choking"},
-				{name: "Food", obj: "Food when starving"},
-				{name: "Water", obj: "Water when thirsty"},
-				{name: "Temperature", obj: "A warm jacket when cold"},
-				{name: "Sleep", obj: "Sleep when sleepy"},
-				{name: "Homeostasis", obj: "Something that will make you vomit when you've taken poison"},
-				{name: "Excretion", obj: "The opportunity to go to the bathroom if you need to go"},
+				{name: "Breathing", obj: "A breath of air when choking", file: "maslow/breathing.svg"},
+				{name: "Food", obj: "Food when starving", file: "maslow/food.svg"},
+				{name: "Water", obj: "Water when thirsty", file: "maslow/water.svg"},
+				{name: "Sex", obj: "Hanky-panky", file: "maslow/sex.svg"},
+				{name: "Sleep", obj: "Sleep when sleepy", file: "maslow/sleep.svg"},
+				{name: "Homeostasis", obj: "Warmth when cold, or coolness when hot", file: "maslow/homeostasis.svg"},
+				{name: "Excretion", obj: "The opportunity to go to the bathroom if you need to go", file: "maslow/excretion.svg"},
 
-				{name: "Security of body", obj: "Safety when you feel unsafe"},
-				{name: "Security of employment", obj: "The ability to keep your job when it's threatened"},
-				{name: "Security of resources", obj: "The ability to get gasoline, water, electricity and other resources when they're scarce"},
-				{name: "Security of morality", obj: "The ability to keep to your faith or system of morality when it's threatened"},
-				{name: "Security of the familiy", obj: "Safety for your family when you feel unsafe"},
-				{name: "Security of health", obj: "The ability to be healthy when sick"},
-				{name: "Security of property", obj: "The ability to keep your belongings when they're threatened"},
+				{name: "Security of body", obj: "Safety when you feel unsafe", file: "maslow/securityofbody.svg"},
+				{name: "Security of employment", obj: "The ability to keep your job when it's threatened", file: "maslow/securityofemployment.svg"},
+				{name: "Security of resources", obj: "The ability to get gasoline, water, electricity and other resources when they're scarce", file: "maslow/securityofresources.svg"},
+				{name: "Security of morality", obj: "The ability to keep to your faith or system of morality when it's threatened", file: "maslow/securityofmorality.svg"},
+				{name: "Security of the familiy", obj: "Safety for your family when you feel unsafe", file: "maslow/securityofthefamily.svg"},
+				{name: "Security of health", obj: "The ability to be healthy when sick", file: "maslow/securityofhealth.svg"},
+				{name: "Security of property", obj: "The ability to keep your belongings when they're threatened", file: "maslow/securityofproperty.svg"},
 
-				{name: "Friendship", obj: "Your friends"},
-				{name: "Family", obj: "Your family"},
-				{name: "Intimacy", obj: "Your loved ones"},
+				{name: "Friendship", obj: "Your friends", file: "maslow/friendship.svg"},
+				{name: "Family", obj: "Your family", file: "maslow/family.svg"},
+				{name: "Intimacy", obj: "Your loved ones", file: "maslow/intimacy.svg"},
 
-				{name: "Self-esteem", obj: "Self-esteem"},
-				{name: "Confidence", obj: "Confidence"},
-				{name: "Achievement", obj: "A sense of achievement"},
-				{name: "Respect of others", obj: "The ability to respect others"},
-				{name: "Respect by others", obj: "The respect of others"},
+				{name: "Self-esteem", obj: "Self-esteem", file: "maslow/selfesteem.svg"},
+				{name: "Confidence", obj: "Confidence", file: "maslow/confidence.svg"},
+				{name: "Achievement", obj: "A sense of achievement", file: "maslow/achievement.svg"},
+				{name: "Respect of others", obj: "The ability to respect others", file: "maslow/respectofothers.svg"},
+				{name: "Respect by others", obj: "The respect of others", file: "maslow/respectbyothers.svg"},
 
-				{name: "Morality", obj: "Your morality"},
-				{name: "Creativity", obj: "Your creativity"},
-				{name: "Spontaneity", obj: "Your spontaneity"},
-				{name: "Problem Solving", obj: "Your ability to solve problems"},
-				{name: "Lack of Prejudice", obj: "Your lack of prejudice"},
-				{name: "Acceptance of Facts", obj: "Acceptance of the facts"}
+				{name: "Morality", obj: "Your morality", file: "maslow/morality.svg"},
+				{name: "Creativity", obj: "Your creativity", file: "maslow/creativity.svg"},
+				{name: "Spontaneity", obj: "Your spontaneity", file: "maslow/spontaneity.svg"},
+				{name: "Problem Solving", obj: "Your ability to solve problems", file: "maslow/problemsolving.svg"},
+				{name: "Lack of Prejudice", obj: "Your lack of prejudice", file: "maslow/lackofprejudice.svg"},
+				{name: "Acceptance of Facts", obj: "Acceptance of the facts", file: "maslow/acceptanceoffacts.svg"}
 			],
 			[
-				{name: "Xbone", obj: "An Xbox One"}, 
-				{name: "PS4", obj: "A Playstation 4"}, 
-				{name: "WiiU", obj: "A Wii U"}, 
-				{name:"rock", obj: "A rock"}
+				{name: "Xbone", obj: "An Xbox One", file:"games/xboxone.png"}, 
+				{name: "PS4", obj: "A Playstation 4", file:"games/ps4logo.png"}, 
+				{name: "WiiU", obj: "A Wii U", file:"games/wiiu.png"}, 
+				{name: "PC", obj: "A good PC", file:"games/steam.png"}
 			],
 			[
-				{name: "Samsung", obj: "A top-of-the-line Samsung phone"},
-				{name: "Apple", obj: "A top-of-the-line Apple phone"},
-				{name: "HTC", obj: "A top-of-the-line HTC phone"},
-				{name: "Motorola", obj: "A top-of-the-line Motorola phone"},
-				{name: "LG", obj: "A top-of-the-line LG phone"},
-				{name: "Nokia", obj: "A top-of-the-line Nokia phone"}
+				{name: "Samsung", obj: "A top-of-the-line Samsung phone", file:"phone/samsung.png"},
+				{name: "Apple", obj: "A top-of-the-line Apple phone", file:"phone/apple.png"},
+				{name: "HTC", obj: "A top-of-the-line HTC phone", file:"phone/htc.png"},
+				{name: "Motorola", obj: "A top-of-the-line Motorola phone", file:"phone/motorola.png"},
+				{name: "LG", obj: "A top-of-the-line LG phone", file:"phone/lg.png"},
+				{name: "Nokia", obj: "A top-of-the-line Nokia phone", file:"phone/nokia.png"}
 			],
 			[
-				{name: "Bulbasaur", obj: "Bulbasaur"},
-				{name: "Charmander", obj: "Charmander"},
-				{name: "Squirtle", obj: "Squirtle"},
-				{name: "Pikachu", obj: "Pikachu"}
+				{name: "Bulbasaur", obj: "Bulbasaur", file:"pokemon/leaf.png"},
+				{name: "Charmander", obj: "Charmander", file:"pokemon/fire.png"},
+				{name: "Squirtle", obj: "Squirtle", file:"water.png"},
+				{name: "Pikachu", obj: "Pikachu", file:"electricity.png"}
 			]
 			];
 var edgeStateInfo = [
